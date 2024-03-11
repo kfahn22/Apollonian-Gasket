da0693a901fc70a1bd68d5665037bcb3c3a92402
Kathy <65121394+kfahn22@users.noreply.github.com>
2024-03-11 06:25:04 -0400

    reorganize folders

---
+++ Gaskets & Steiner Chains/gasket-mixed-original/sketch.js
@@ -0,0 +1,70 @@
+let chains = [];
+let gaskets = [];
+let colorsCT = [
+  "#70327E",
+  "#9253A1",
+  "#A42963",
+  "#EC015A",
+  "#F063A4",
+  "#F16164",
+  "#F89E4F",
+  "#2DC5F4",
+];
+
+function setup() {
+  createCanvas(400, 400);
+  let col = color("#F063A4");
+  gaskets.push(new Gasket(width / 2, width / 2, height / 2, 3, col));
+  let circleArray = gaskets[0].allCircles;
+  let len = circleArray.length;
+  for (let i = 0; i < len / 2; i++) {
+    // choose a random gasket to replace
+    let index = int(random(1, circleArray.length));
+    let c = circleArray[index];
+
+    // Replace a gasket with a chain
+    chains.push(new SteinerChain(c.radius, c.center.a, c.center.b, 1, col));
+    // Find the original gasket and delete
+    circleArray.splice(index, 1);
+  }
+
+  for (let n = 0; n < 1; n++) {
+    for (let i = gaskets.length - 1; i >= 0; i--) {
+      let nextG = gaskets[i].recurse();
+      if (nextG) gaskets.push(...nextG);
+    }
+  }
+  console.log(chains.length);
+  let perc = 0.98;
+  for (let i = 0; i < chains.length * perc; i++) {
+    let newArray = chains[i].allCircles;
+    for (let j = 0; j < newArray.length; j++) {
+      let c = newArray[j];
+      if (c.radius > 10) {
+        chains.push(new SteinerChain(c.radius, c.center.a, c.center.b, 2, col));
+      }
+    }
+  }
+}
+
+function draw() {
+  background("#9253A1");
+
+  scale(1);
+
+  for (let gasket of gaskets) {
+    gasket.show();
+    //gasket.nextGeneration();
+  }
+  //translate(width/2, height/2)
+  for (let chain of chains) {
+    push();
+    //translate(chain.x, chain.y);
+    chain.show();
+    pop();
+  }
+}
+
+function mousePressed() {
+  save("mix.jpg");
+}
