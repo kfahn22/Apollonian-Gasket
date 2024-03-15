fcfba474710029276420a1362c3dac6a621a443a
Kathy <65121394+kfahn22@users.noreply.github.com>
2024-03-15 06:48:10 -0400

    back to original with mixed gasket

---
+++ Gaskets & Steiner Chains/WIP/adjusting n - mistake copy/layered-flower-attempt/sketch.js
@@ -0,0 +1,82 @@
+let chains = [];
+let gaskets = [];
+let colorsCT = [
+  "#70327E",
+  "#9253A1",
+  "#A42963",
+  "#EC015A",
+  "#F063A4",
+  "#F16164",
+  "#F89E4F",
+  "#2DC5F4",
+];
+// Adjustment factor for different number of initial circles
+let dict = [
+  { key: "3", value: "0.46" },
+  { key: "4", value: "0.41" },
+  { key: "5", value: "0.37" },
+  { key: "6", value: "0.33" },
+  { key: "7", value: "0.3" },
+  { key: "8", value: "0.27" },
+  { key: "9", value: "0.25" },
+  { key: "10", value: "0.23" },
+  { key: "11", value: "0.22" },
+  { key: "12", value: "0.2" },
+];
+let n = 9;
+
+function setup() {
+  createCanvas(400, 400);
+  let col = color("#2DC5F4");
+  // console.log(dict[key=="3"]);
+  // r, x, y, n, adj, color
+  chains.push(
+    new SteinerChain(
+      width / 2,
+      width / 2,
+      height / 2,
+      12,
+      col
+    )
+  );
+
+  let circleArray = chains[0].allCircles;
+  let len = circleArray.length;
+  for (let i = 0; i < len; i++) {
+    let c = circleArray[i];
+    //console.log(c);
+    // Replace a gasket with a chain
+
+    gaskets.push(
+      new Gasket(c.center.a, c.center.b, c.radius, i+1, 3)
+    );
+  }
+
+  for (let n = 0; n < 1; n++) {
+    for (let i = gaskets.length - 1; i >= 0; i--) {
+      let nextG = gaskets[i].recurse();
+      if (nextG) gaskets.push(...nextG);
+    }
+  }
+}
+
+function draw() {
+  background(0);
+
+  scale(1);
+  
+  for (let i = 0; i < gaskets.length; i++) {
+    gaskets[i].show();
+  }
+  // push()
+  // noFill()
+  // //chains[0].show();
+  for (let i = 1; i < chains.length; i++) {
+    chains[i].show();
+    
+  }
+  // stroke(31,19,0);
+  // strokeWeight(3);
+  // //circle(width / 2, height / 2, (width / 2) * 2);
+  // pop();
+}
---
+++ Gaskets & Steiner Chains/WIP/try formula again/steiner.js
@@ -0,0 +1,184 @@
+// https://github.com/CodingTrain/Logo-Animations/tree/main/public/gasketLogo
+
+//const epsilon = 0.1;
+
+function isTangent(c1, c2) {
+  let d = c1.dist(c2);
+  let r1 = c1.radius;
+  let r2 = c2.radius;
+
+  let a = abs(d - (r1 + r2)) < epsilon;
+  let b = abs(d - abs(r2 - r1)) < epsilon;
+  return a || b;
+}
+
+function validate(c4, c1, c2, c3, allCircles) {
+  if (c4.radius < 2) return false;
+
+  for (let other of allCircles) {
+    let d = c4.dist(other);
+    let radiusDiff = abs(c4.radius - other.radius);
+    if (d < epsilon && radiusDiff < epsilon) {
+      return false;
+    }
+  }
+
+  // Check if all 4 circles are mututally tangential
+  if (!isTangent(c4, c1)) return false;
+  if (!isTangent(c4, c2)) return false;
+  if (!isTangent(c4, c3)) return false;
+
+  return true;
+}
+
+class SteinerChain {
+  constructor(_r, _x, _y, offset, sw, color) {
+    this.r = _r;
+    this.x = _x;
+    this.y = _y;
+    this.offset = offset;
+    this.allCircles = [];
+    this.queue = [];
+    this.n = 4;
+    let c1 = new GasketCircle(-1 / this.r, this.x, this.y);
+    let theta = PI/this.n;
+    let r2 = (this.r * sin(theta))/(1+ sin(0));
+    let v = p5.Vector.fromAngle(random(TWO_PI));
+    v.setMag(this.r - r2);
+    let c2 = new GasketCircle(1 / r2, this.x + this.offset, this.y+ this.offset);
+    let r3 = v.mag() - r2;
+    this.allCircles.push(c1, c2);
+    this.queue = [c1, c2];
+    for (let i = 0; i < this.n; i++) {
+      v.rotate(TWO_PI / this.n);
+      v.setMag(c1.radius - r3);
+      let c = new GasketCircle(1 / r3, this.x + v.x + this.offset, this.y + v.y + this.offset);
+      this.allCircles.push(c);
+      this.queue.push(c);
+      //console.log(this.queue);
+    }
+    this.color = color;
+    this.recursed = false;
+    this.startC = this.allCircles.shift();
+
+    //console.log(this.startC)
+    this.sw = sw;
+
+    let len = -1;
+    while (this.allCircles.length !== len) {
+      len = this.allCircles.length;
+      this.nextGeneration();
+    }
+  }
+
+  recurse() {
+    if (this.recursed) return;
+    this.recursed = true;
+    let newChains = [];
+    for (let i = 0; i < this.allCircles.length; i++) {
+      let c = this.allCircles[i];
+      if (c.radius < 4) continue;
+      newChains.push(
+        new SteinerChain(
+          c.center.a,
+          c.center.b,
+          c.radius,
+          this.sw / 16,
+          this.color
+        )
+      );
+    }
+    return newChains;
+  }
+
+  nextGeneration() {
+    let nextQueue = [];
+    nextQueue = nextQueue.concat(this.queue);
+    this.queue = nextQueue;
+  }
+
+  show() {
+    for (let c of this.allCircles) {
+      c.show(this.color, this.sw);
+    }
+  }
+}
+
+function complexDescartes(c1, c2, c3, k4) {
+  let k1 = c1.bend;
+  let k2 = c2.bend;
+  let k3 = c3.bend;
+  let z1 = c1.center;
+  let z2 = c2.center;
+  let z3 = c3.center;
+
+  let zk1 = z1.scale(k1);
+  let zk2 = z2.scale(k2);
+  let zk3 = z3.scale(k3);
+  let sum = zk1.add(zk2).add(zk3);
+
+  let root = zk1.mult(zk2).add(zk2.mult(zk3)).add(zk1.mult(zk3));
+  root = root.sqrt().scale(2);
+  let center1 = sum.add(root).scale(1 / k4[0]);
+  let center2 = sum.sub(root).scale(1 / k4[0]);
+  let center3 = sum.add(root).scale(1 / k4[1]);
+  let center4 = sum.sub(root).scale(1 / k4[1]);
+
+  return [
+    new GasketCircle(k4[0], center1.a, center1.b),
+    new GasketCircle(k4[0], center2.a, center2.b),
+    new GasketCircle(k4[1], center3.a, center3.b),
+    new GasketCircle(k4[1], center4.a, center4.b),
+  ];
+}
+
+function descartes(c1, c2, c3) {
+  let k1 = c1.bend;
+  let k2 = c2.bend;
+  let k3 = c3.bend;
+
+  let sum = k1 + k2 + k3;
+  let product = abs(k1 * k2 + k2 * k3 + k1 * k3);
+  let root = 2 * sqrt(product);
+  return [sum + root, sum - root];
+}
+
+// class Circle {
+//   constructor(bend, x, y) {
+//     this.center = new Complex(x, y);
+//     this.bend = bend;
+//     this.radius = abs(1 / this.bend);
+//   }
+
+//   show(c, sw) {
+//     stroke(c);
+//     let sw2 = map(this.radius, 0, width / 2, 1, 20);
+//     strokeWeight(sw2);
+//     noFill();
+//     circle(this.center.a, this.center.b, this.radius * 2);
+//   }
+
+//   dist(other) {
+//     return dist(this.center.a, this.center.b, other.center.a, other.center.b);
+//   }
+// }
+
+class ChainCircle {
+  constructor(bend, x, y) {
+    this.center = new Complex(x, y);
+    this.bend = bend;
+    this.radius = abs(1 / this.bend);
+  }
+
+  show(c, sw) {
+    stroke(c);
+    let sw2 = map(this.radius, 0, width / 2, 1, 5);
+    strokeWeight(sw2);
+    noFill();
+    circle(this.center.a, this.center.b, this.radius * 2);
+  }
+
+  dist(other) {
+    return dist(this.center.a, this.center.b, other.center.a, other.center.b);
+  }
+}
